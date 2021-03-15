620a671b00f3fbba0650d4d9080d22016c12f712
s5121191 <80707879+s5121191@users.noreply.github.com>
2021-03-15 20:06:23 +0000

    Add files via upload

--- cairis/test/test_Models.py
+++ cairis/test/test_Models.py
@@ -49,6 +49,7 @@ class ModelTests(unittest.TestCase):
     b.get_dbproxy().assumptionPersonaModel('Claire')
     b.get_dbproxy().textualArgumentationModel('Claire','Activities')
     
+    
     importModelFile(os.environ['CAIRIS_SRC'] + '/../examples/exemplars/ACME_Water/ACME_Water.xml',1,'test')
     importLocationsFile(os.environ['CAIRIS_SRC'] + '/../examples/exemplars/ACME_Water/PooleWWTW.xml','test')
     b.get_dbproxy().locationsRiskModel('PooleWWTW','Day')
